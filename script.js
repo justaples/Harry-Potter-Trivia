447e9b4f878639025ca9cf4bddceaa8da6c0f6d2
justaples <juhpaim@hotmail.com>
2022-04-28 18:08:35 -0400

    Added hover to buttons on css, changed font, still have not decided what's best background color, will decide later

--- script.js
+++ script.js
@@ -1,1 +1,54 @@
 console.log('hello')
+let questionsAndAnswers = [
+    {
+        question:'Who teaches Harry how to play Wizard’s chess?',
+        answer: {
+            a:'Hermione',
+            b:'Ron',
+            c:'Hagrid',
+            d:'Dudley',
+        },
+        correctAnswer: 'b'
+    },
+    {
+        question:'When is Harry Potter’s birthday?',
+        answer: {
+            a:'December 31st',
+            b:'July 31st',
+            c:'July 31st',
+            d:'March 31st',
+        },
+        correctAnswer:'b'
+    },
+    {
+        question:'Who teaches History of Magic at Hogwarts?',
+        answer: {
+            a:'Professor Flitwick',
+            b:'Professor Sprout',
+            c:'Professor Vector',
+            d:'Professor Binns',
+        },
+        correctAnswer:'d'
+    },
+    {
+        question:'',
+        answer: {
+            a:'',
+            b:'',
+            c:'',
+            d:'',
+        },
+        correctAnswer:''
+    },
+    {
+        question:'',
+        answer: {
+            a:'',
+            b:'',
+            c:'',
+            d:'',
+        },
+        correctAnswer:''
+    },
+    
+]
