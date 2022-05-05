2aa47463c5be65e8292d01a0e636a60642f7bca1
justaples <juhpaim@hotmail.com>
2022-05-05 08:28:02 -0400

    Play again button was added at the end page - Pseudocode and reference links were added to the code. Removed all console logs. Fixed Indentation.

--- README.md
+++ README.md
@@ -1,12 +1,11 @@
-# project-js-game
+# Harry Potter Trivia
 
 ### Description
-
-
-
+This is a trivia for all the Potterheads to test if they are Pure-blood, Half- Blood, or a Squib. This game is full of sounds effects and background songs. The player is shown a Harry Potter related question and 4 different possible answers on screen. There are a total of 10 questions and each correct answer awards 10 points. If the player gets 80 points or more, they're a Pure-Blood Potterhead, 50 to 70 points, they're considered a Half-Blood Potterhead, and 40 points or less they're a Squib. 
 
 ### Screenshot of the game
 
+- ********* post screenshots here *************
 
 
 ### Technologies Used
@@ -16,10 +15,14 @@
 
 ### Getting Started
 
-- post the link to deployed game
-- instruction to user
+- ********* post the link to deployed game *********
 
-
+- Instructions to player:
+    - Once the main page loads, hit start;
+    - You will see a total of 10 questions as you go through the next pages;
+    - Think before choosing an answer, once you select it, you can't change it;
+    - Each correct answer will award 10 points;
+    - The final result will be displayed at the final page;
 
 
 ### User Stories
@@ -46,14 +49,23 @@
 
 ### Planned Future Enhacements
 
-- put stretch goals not added to game 
+- Add a button to turn sound effects on and off;
+- Add a button to the first page to show instructions;
+- Add difficulty level, so the player can choose between, easy, medium and hard;
+- Add a timer to each question;
+- Add the possibility to get hints to each question. A maximum of 3 hints per game;
+- Add accessibility for people with disability to the game, so the questions and answers are read out loud;
 
 ### Wireframes
 
 <IMG SRC = "./images/UPLOAD THIS First Page - Trivia Question.png">
 
 
-### Unsolved problems / Major hurdles
+### Unsolved problems 
+
 - Randomizing questions when hitting "next" button
-- Disabling answer buttons after selecting a wrong answer
+
+### Major hurdles
+
+- Disabling all the answer buttons after an answer has been selected. So the player can't cheat and get extra points or select the right answer after choosing the wrong one.
     
--- script.js
+++ script.js
@@ -1,6 +1,10 @@
+/*=======================================================================
+Retrieving HTML elements using DOM manipulation
+=======================================================================*/
+
 let getScore = document.querySelector('.score');
 let getScoreDiv = document.querySelector('.score-div');
-let getQuestionCounter = document.querySelector('.counter')
+let getQuestionCounter = document.querySelector('.counter');
 let getQuestion = document.querySelector('.question');
 let getAnswers = document.querySelectorAll('.option');
 let getAnswer1 = document.querySelector('.option1');
@@ -17,7 +21,11 @@ let getFinalMessage = document.querySelector('.final-message');
 let getStartPage = document.querySelector('.start-page');
 let getStartBtn = document.querySelector('.start');
 let getHPTitle = document.querySelector('.hp-title');
+let getPlayAgain = document.querySelector('.play-again');
 
+/*=======================================================================
+Entering trivia's questions and answers into an array of objects
+=======================================================================*/
 let questionsAndAnswers = [
     {
         question:'Who teaches Harry how to play Wizard’s chess?',
@@ -36,7 +44,7 @@ let questionsAndAnswers = [
     },   
     {
         question:'Who said this?\n“I am good looking enough for both of us"',
-        options: ['Draco Malfoy','Ron Weasley','Fleur Delacour','Ginny Weasley'],
+        options: ['Fleur Delacour', 'Draco Malfoy','Ron Weasley','Ginny Weasley'],
         correctAnswer: 'Fleur Delacour'
     },   
     {
@@ -56,7 +64,7 @@ let questionsAndAnswers = [
     },   
     {
         question: "What is the name of Hagrid's Hippogriff?",
-        options: ['Scabbers','Pickett','Teddy','Buckbeak'],
+        options: ['Buckbeak','Scabbers','Pickett','Teddy'],
         correctAnswer: 'Buckbeak'
     },   
     {
@@ -73,121 +81,198 @@ let questionsAndAnswers = [
         question:'',
         options: ['','','',''],
         correctAnswer: ''
-    },   
+    }   
 ]
 
+/*=======================================================================
+Adding variables for the audios and setting up a volume
+
+- Video referencing how to add audio to the code:
+    https://www.youtube.com/watch?v=IlnZxLeQezI 
+- Link referencing how to set up loop() to an audio:
+    https://www.w3schools.com/jsref/prop_audio_loop.asp 
+- Sound effects downloaded from:
+    https://www.101soundboards.com/boards/26424-harry-potter-and-the-sorcerers-stone-soundboard 
+=======================================================================*/
+
 const audioStart = new Audio("./Sound effect/Hedwig-theme.mp3");
-audioStart.volume = 0.008;
+audioStart.volume = 0.04;
+/* Allowing main song play in a loop */
 audioStart.loop = true;
 const audioAlohomora = new Audio('./Sound effect/Alohomora1.mp3');
 audioAlohomora.volume = 0.05
 const audioRight = new Audio('./Sound effect/10-points to gryffindor.mp3');
-audioRight.volume= 0.01;
+audioRight.volume= 0.05;
 const audioWrong = new Audio('./Sound effect/Olivander.mp3');
-audioWrong.volume= 0.01;
-const audioEnd = new Audio('./Sound effect/End.mp3');
-audioEnd.volume = 0.1;
+audioWrong.volume= 0.02;
+const audioGoodEnd = new Audio('./Sound effect/End.mp3');
+audioGoodEnd.volume = 0.1;
+const audioBadEnd = new Audio('./Sound effect/Squib-result.mp3');
+audioBadEnd.volume = 0.1;
+
+/*=======================================================================
+Adding a function so that when the start button is clicked on the first page, 
+audio starts playing and the first page gets a class of .hide added to it 
+and the main trivia has the class of .hide removed
+
+- Video referencing how to code using hide class:
+    https://www.youtube.com/watch?v=riDzcEQbX6k
+=======================================================================*/
 
 const startGame = () =>{
     getStartBtn.addEventListener('click', e =>{
         
         audioAlohomora.play();
+        
+        /*=======================================================================
+        setTimeOut was added so that the answers will show up after a 1.5 s, 
+        matching the "Alohomora" sounds effect
+        =======================================================================*/
         setTimeout(function(){
+            
+            /*=======================================================================
+            2nd setTimeOut added so that the main song starts playing after "Alohomora" is played
+            =======================================================================*/
             setTimeout(function(){
-                audioStart.play()
-            },1200)
+                audioStart.play();
+            },1200);
             getStartPage.style.display = "none";
             getStartPage.classList.add('hide');
             getMainTop.classList.remove('hide');
             getEndPage.classList.remove('hide');
-        },1500)
-    })
+        },1500);
+    });
 }
 startGame();
 
-let turn = 9;
-let removed = false;
+/*=======================================================================
+Setting a variable called "turn" starting at 0, so that when it's called later, 
+it'll start from the first index of the questionsAndAnswers array
+=======================================================================*/
+
+let turn = 0;
+
+
+/*=======================================================================
+Function that handles the logic of the game when an answer is clicked
+=======================================================================*/
+
 const handleClick = (e) => {
     if(e.target.innerText === questionsAndAnswers[turn].correctAnswer){
         audioRight.play();
         showResult.innerHTML = "Correct! 10 points to Gryffindor!";
-        getScore.innerHTML = `${parseInt(getScore.innerHTML) + 60}`;
-        getNext.classList.remove('hide')
-        getResultContainer.classList.remove('hide')    
+        getScore.innerHTML = `${parseInt(getScore.innerHTML) + 10}`;
+        getNext.classList.remove('hide');
+        getResultContainer.classList.remove('hide');    
     }else{
         audioWrong.play();
         showResult.innerHTML = "Nope, definitely not!!";
         getResultContainer.style.alignItems = 'center';
     }
     
-    getNext.classList.remove('hide')
-    getResultContainer.classList.remove('hide')    
+    getNext.classList.remove('hide');
+    getResultContainer.classList.remove('hide');    
+    
+    /*=======================================================================
+    Adding removeEventListener to disable any clicking after choosing an answer, so that once a wrong question is selected, it won't allow you to choose a correct one afterwards, or if a correct answer is selected, it won't allow you to keep clicking and getting more point added to the score
+    =======================================================================*/
     getAnswers.forEach(item =>{
         item.removeEventListener('click',
-        handleClick,false    
-        )
-    })
+        handleClick    
+        );
+    });
 }
+
+/*=======================================================================
+Function to bring the questions and answers to the screen according to the turn number
+=======================================================================*/
 
 const playTrivia = (turn) => {
     getAnswers.forEach(item => {
         item.addEventListener('click', 
-        handleClick    
-        ,{once:true})
-    })
-    if(removed){
-        getAnswers.forEach(item =>{
-            item.removeEventListener('click',
-            handleClick,false    
-            )
-        })
-    }
-    getQuestionCounter.innerHTML = `<h4>Question ${turn+1} out of 10</h4>`
-    getQuestionCounter.style.margin = "0px"
+        handleClick
+        );
+    });
+    getQuestionCounter.innerHTML = `<h4>Question ${turn+1} out of 10</h4>`;
+    getQuestionCounter.style.margin = "0px";
     getQuestion.innerText = questionsAndAnswers[turn].question;
     getAnswer1.innerHTML = questionsAndAnswers[turn].options[0];
     getAnswer2.innerHTML = questionsAndAnswers[turn].options[1];
     getAnswer3.innerHTML = questionsAndAnswers[turn].options[2];
     getAnswer4.innerHTML = questionsAndAnswers[turn].options[3];
-    }
+}
 playTrivia(turn);
+
+
+/*=======================================================================
+- Function to show next question when the button Next is clicked, invoking the playTrivia() function. 
+    - When question is displayed, the class of .hide is added to the "next" button, so it is not displayed until an answer is selected
+    - When question is displayed, the class of .hide is also added to the "result container", so it's not displayed until an answers is selected
+=======================================================================*/
 
 const showNextQuestion = () =>{
     getNext.addEventListener('click', e =>{
-        getNext.classList.add('hide')
-        getResultContainer.classList.add('hide')
+        getNext.classList.add('hide');
+        getResultContainer.classList.add('hide');
         turn ++
-        playTrivia(turn)
-        console.log(turn)
+        playTrivia(turn);
+        
+        /*=======================================================================
+        Adding a conditional to finish the game when question #10 is reached - 11th index of the array
+        =======================================================================*/
         if(turn===10){
-            audioStart.pause()
-            audioEnd.play()
-            getEndPage.classList.add('hide')
-            getFinalMessage.classList.remove('hide')
+            audioStart.pause();
+            getEndPage.classList.add('hide');
+            getFinalMessage.classList.remove('hide');
+            getPlayAgain.classList.remove('hide');
+            
+            /*=======================================================================
+            Adding conditionals to display different messages and different songs according to the final score.
+            =======================================================================*/
             if(getScore.innerHTML >= 80){
-                getFinalMessage.innerHTML = "<h2>Congratulations<br>You're a Pure-Blood Potterhead!!!</h2>"
+                audioGoodEnd.play();
+                getFinalMessage.innerHTML = "<h2>Congratulations<br>You're a Pure-Blood Potterhead!!!</h2>";
             }else if (getScore.innerHTML >=50 && getScore.innerHTML < 80){
-                getFinalMessage.style.fontSize = "15px"
-                getFinalMessage.innerHTML = "<h2>You are a Half-Blood Potterhead! <br>This calls for a Harry Potter movie marathon<br>to brush up on your knowledge!!</h2>"
+                audioGoodEnd.play();
+                getFinalMessage.style.fontSize = "15px";
+                getFinalMessage.innerHTML = "<h2>You are a Half-Blood Potterhead! <br>This calls for a Harry Potter movie marathon<br>to brush up on your knowledge!!</h2>";
             }else{
-                getFinalMessage.innerHTML = "<h2>You're a Squib!<br>Try again.</h2>"
-            }
-            getMainTop.style.display = "flex"
-            getMainTop.style.flexDirection = "column"
-            getMainTop.style.alignContent = "center"
-            getMainTop.style.alignItems = "center"
-            getMainTop.style.height = "730px"
-            getScore.style.fontSize = "150px"
-            getScoreDiv.style.margin = "100px"
-            getScoreDiv.style.marginTop = "60px"
-            getMainTitle.style.fontSize = "50px"
-            getMainTitle.style.color = "#eeba30"
-            getMainTitle.style.textShadow = "3px 3px #946b2d"
-            getMainTitle.style.marginTop = "60px"
-            getHPTitle.style.fontSize = "65px"
-
-        }
-    })
+                audioBadEnd.play();
+                getFinalMessage.innerHTML = "<h2>You're a Squib!<br>Try again.</h2>";
+            };
+            
+            /*=======================================================================
+            Styling the end page using DOM manipulation, since all pages were done using the same HTML
+            =======================================================================*/
+            getMainTop.style.display = "flex";
+            getMainTop.style.flexDirection = "column";
+            getMainTop.style.alignContent = "center";
+            getMainTop.style.alignItems = "center";
+            getMainTop.style.height = "730px";
+            getScore.style.fontSize = "150px";
+            getScoreDiv.style.margin = "100px";
+            getScoreDiv.style.marginTop = "60px";
+            getMainTitle.style.fontSize = "50px";
+            getMainTitle.style.color = "#eeba30";
+            getMainTitle.style.textShadow = "3px 3px #946b2d";
+            getMainTitle.style.marginTop = "60px";
+            getHPTitle.style.fontSize = "65px";
+            
+        };
+    });
 }
 showNextQuestion();
 
+/*=======================================================================
+Function to refresh the page when "Play again" button is clicked
+
+- Link referencing how to use reload()
+    https://www.w3schools.com/jsref/met_loc_reload.asp
+=======================================================================*/
+
+const reset = () => {
+    getPlayAgain.addEventListener('click', e =>{
+        location.reload();
+    })
+}
+reset();
